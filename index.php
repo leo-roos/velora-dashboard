d55505f474468be8fee7bae3f4a0511a4da5ae68
Leo Roos <leoroos88@gmail.com>
2026-09-17 13:49:47 +0200

    move function to functions.php and cleanup code

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 require __DIR__ . "/config.php";
+require __DIR__ . "/functions.php";
+
 session_start();
 ?>
 
@@ -11,47 +13,24 @@ session_start();
     <title>Velora Dashboard</title>
 </head>
 <body>
-    <?php
-    $params = [
-        'client_id' => OAUTH2_CLIENT_ID,
-        'redirect_uri' => REDIRECT_URL,
-        'response_type' => 'code',
-        'scope' => 'identify email guilds.join'
-    ];
-
-    $authorize_url = 'https://discord.com/api/oauth2/authorize?' . http_build_query($params);
-
-	function is_animated($avatar)
-	{
-		$ext = substr($avatar, 0, 2);
-		if ($ext == "a_")
-		{
-			return ".gif";
-		}
-		else
-		{
-			return ".png";
-		}
-	}
-
-    ?>
-
 	<a href="<?php echo $authorize_url; ?>">Logga in med Discord</a>
+
     <?php
 		if (!isset($_SESSION['user'])) {
-            ?>
-            <?php
+	?>
+		<!-- elements shown when not logged in -->
+	<?php
 		}
 		else {
     ?>
-			<h2> User Details :</h2>
-			<p> Name : <?php echo $_SESSION['username'] . '#' . $_SESSION['discrim']; ?></p>
-			<p> ID : <?php echo $_SESSION['user_id']; ?></p>
-			<?php
+		<h2> User Details :</h2>
+		<p> Name : <?php echo $_SESSION['username'] . '#' . $_SESSION['discriminator']; ?></p>
+		<p> ID : <?php echo $_SESSION['user_id']; ?></p>
+		<?php
 			if (isset($_SESSION['email'])) {
 				echo '<p> Email: ' . $_SESSION['email'] . '</p>';
 			}
-			?>
+		?>
 
 			<p> Profile Picture : <img src="https://cdn.discordapp.com/avatars/<?php $extention = is_animated($_SESSION['user_avatar']);
 																				echo $_SESSION['user_id'] . "/" . $_SESSION['user_avatar'] . $extention; ?>" /></p>
