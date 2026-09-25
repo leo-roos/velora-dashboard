1c0bc8f8b2cda78cecda3aafefe1dd6785b9a2b1
Leo <leoroos88@gmail.com>
2026-09-25 20:10:06 +0200

    Add dashboard header and update dashboard nav icons

---
+++ includes/dashboard/header.php
@@ -0,0 +1,8 @@
+<div class="header">
+    <div class="left">
+        <i class="fa-light fa-table-columns"></i>
+        <div class="breadcrumbs">
+            Dashboard
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -25,23 +25,23 @@ include __DIR__ . "/includes/header.php";
 		</div>
 		<nav class="navigation">
 			<a href="<?php echo BASE_URL . "/" ?>" class="item">
-				<i class="fa-solid fa-chart-pie"></i>
+				<i class="fa-light fa-table-columns"></i>
 				<div class="label">Dashboard</div>
 			</a>
 			<a href="<?php echo BASE_URL . "/?page=analytics" ?>" class="item">
-				<i class="fa-solid fa-chart-line network"></i>
+				<i class="fa-light fa-chart-line network"></i>
 				<div class="label">Analytics</div>
 			</a>
 			<a href="<?php echo BASE_URL . "/?page=players" ?>" class="item">
-				<i class="fa-solid fa-users"></i>
+				<i class="fa-light fa-users"></i>
 				<div class="label">Players</div>
 			</a>
 			<a href="<?php echo BASE_URL . "/?page=logbook" ?>" class="item">
-				<i class="fa-solid fa-book-open"></i>
+				<i class="fa-light fa-book-open"></i>
 				<div class="label">Logbook</div>
 			</a>
 			<a href="<?php echo BASE_URL . "/?page=settings" ?>" class="item">
-				<i class="fa-solid fa-sliders"></i>
+				<i class="fa-light fa-sliders"></i>
 				<div class="label">Settings</div>
 			</a>
 		</nav>
@@ -64,8 +64,25 @@ include __DIR__ . "/includes/header.php";
 	</div>
 	<main class="dashboard">
 		<?php
-			$page = $_GET['page'] ?? "main";
-			include_once(__DIR__ . "/includes/dashboard/" . $page . ".php");
+			$pages = [
+				'main' => 'main.php',
+				'analytics' => 'analytics.php',
+				'players' => 'players.php',
+				'logbook' => 'logbook.php',
+				'settings' => 'settings.php',
+			];
+
+			$page = $_GET['page'] ?? 'main';
+
+			if (!isset($pages[$page])) {
+				$page = 'main';
+				// header('Location: '. BASE_URL . '/404.php');
+			}
+
+			include_once __DIR__ . '/includes/dashboard/header.php';
+			echo '<div class="content">';
+			include_once __DIR__ . '/includes/dashboard/' . $pages[$page];
+			echo '</div>';
 		?>
 	</main>
 </div>
